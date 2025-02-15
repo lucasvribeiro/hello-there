5634ab4c3522fdfa5b37027d8b8611b628793d55
Lucas Ribeiro <lucasvribeiro14@gmail.com>
2025-02-15 16:08:49 -0300

    (feat) create useColor hook

---
+++ hooks/useColor.ts
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+import {
+  saveColor,
+  getColor,
+  getColorsHistory,
+  clearColorsHistory
+} from '../storage/asyncStorage'
+import { Color, ColorsHistory } from '../types'
+import { DEFAULT_COLOR } from '../constants/DefaultColor'
+
+const useColor = () => {
+  const [color, setColor] = useState<Color>(DEFAULT_COLOR)
+  const [colorsHistory, setColorsHistory] = useState<ColorsHistory>([])
+
+  useEffect(() => {
+    const fetchColorData = async () => {
+      const savedColor = await getColor()
+      const savedColorsHistory = await getColorsHistory()
+
+      if (savedColor) setColor(savedColor)
+      if (savedColorsHistory) setColorsHistory(savedColorsHistory)
+    }
+
+    fetchColorData()
+  }, [])
+
+  const changeColor = async (newColor: Color) => {
+    try {
+      await saveColor(color)
+      setColor({ ...newColor })
+
+      if (!colorsHistory.some((color) => color.hex === newColor.hex)) {
+        const updatedHistory = await getColorsHistory()
+        setColorsHistory([...updatedHistory])
+      }
+    } catch (error) {
+      console.error('Error changing color:', error)
+    }
+  }
+
+  const clearColors = async () => {
+    try {
+      await clearColorsHistory()
+      setColorsHistory([])
+      setColor(DEFAULT_COLOR)
+    } catch (error) {
+      console.error('Error clearing colors:', error)
+    }
+  }
+
+  return {
+    color,
+    colorsHistory,
+    changeColor,
+    clearColors
+  }
+}
+
+export default useColor
